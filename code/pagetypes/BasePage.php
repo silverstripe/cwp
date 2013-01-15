6f0172c1258a3cb321563b3a43c687570fafb71f
Hamish Friedlander <hamish@silverstripe.com>
2013-01-15 16:49:09 +1300

    FIX Hide BasePage from CMS

--- code/pagetypes/BasePage.php
+++ code/pagetypes/BasePage.php
@@ -2,6 +2,9 @@
 
 class BasePage extends SiteTree {
 
+	// Hide this page type from the CMS. hide_ancestor is slightly misnamed, should really be just "hide"
+	static $hide_ancestor = 'BasePage';
+
 }
 
 class BasePage_Controller extends ContentController {
