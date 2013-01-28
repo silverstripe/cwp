b42ae027680be05a1bb7beb36c90bbb5e6e221ec
Robert Curry <robert@silverstripe.com>
2013-01-29 09:33:39 +1300

    Add lists module to format nested ol/ul properly

--- _config.php
+++ _config.php
@@ -90,6 +90,7 @@ $cwpEditor->setOptions(array(
 
 $cwpEditor->enablePlugins('media', 'fullscreen', 'inlinepopups');
 $cwpEditor->enablePlugins('template');
+$cwpEditor->enablePlugins('lists');
 $cwpEditor->enablePlugins('visualchars');
 $cwpEditor->enablePlugins('xhtmlxtras');
 $cwpEditor->enablePlugins(array(
