43ebe9d52f3bc5eb39a63ccc78d7ad8a4cb31bb8
Robert Curry <robert@silverstripe.com>
2013-01-31 17:14:12 +1300

    Add test for home page PDF link

--- tests/BasePageTests.php
+++ tests/BasePageTests.php
@@ -20,10 +20,14 @@ class BasePageTests extends SapphireTest {
 		$this->assertContains('test-page-one/downloadpdf', $page->PdfLink(), 'Link to download PDF');
 	}
 
+	public function testHomePagePdfLink() {
+		$page = $this->objFromFixture('Page', 'home-page');
+		$this->assertContains('home/downloadpdf', $page->PdfLink(), 'Link to download PDF');
+	}
+
 	public function testPdfLinkDisabled() {
 		BasePage::$pdf_export_enabled = false;
 		$page = $this->objFromFixture('Page', 'test-page-one');
 		$this->assertFalse($page->PdfLink(), 'No PDF link as the functionality is disabled');
 	}
-
 }
