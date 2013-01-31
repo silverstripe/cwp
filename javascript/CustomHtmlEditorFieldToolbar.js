1b92b3dfe3b24b45fae4f7e6d91201bd96dbfaf0
Mateusz Uzdowski <mateusz@silverstripe.com>
2013-01-31 13:54:36 +1300

    Suggest the alt text rather than require for embedding images.
    Add some comments.

--- javascript/CustomHtmlEditorFieldToolbar.js
+++ javascript/CustomHtmlEditorFieldToolbar.js
@@ -1,31 +1,46 @@
 (function($) {
 	$.entwine('ss', function($) {
+
 		$('form.htmleditorfield-mediaform').entwine({
+			/**
+			 * Validate the form for the presence of the alt attribute.
+			 */
 			onsubmit: function() {
 				var items = $('div.ss-uploadfield-files .ss-uploadfield-item');
 
+				// Look through the current file items and validate each.
 				for (var i = 0; i < items.length; i++) {
 					var self = $(items[i]);
 					var input = self.find('input[name="AltText"]');
 					var container = self.find('.ss-uploadfield-item-editform');
 
+					// Check for the presence of Alt.
 					if (input.length == 0) {
 						continue;
 					}
 
+					// Collapse all items, so we can highlight the invalid ones.
 					container.height(0);
+
+					// Reset the borders for the ones that have already been fixed.
 					input.css('border', '1px solid #b3b3b3');
 
+					// Validate
 					if (input.val().length == 0) {
+						// Mark field as invalid.
 						container.height('auto');
 						input.css('border', '1px solid red');
-						alert(
+
+						// Ask the user to confirm proceeding.
+						var response = confirm(
 							ss.i18n.sprintf(
 								ss.i18n._t('CWP.SPECIFYALTTEXT'),
 								$.trim(self.find('.ss-uploadfield-item-name span').html())
 							)
 						);
-						return false;
+						if (response===false) {
+							return false;
+						}
 					}
 				}
 
--- javascript/lang/en_US.js
+++ javascript/lang/en_US.js
@@ -2,6 +2,6 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
 	if(typeof(console) != 'undefined') console.error('Class ss.i18n not defined');
 } else {
 	ss.i18n.addDictionary('en_US', {
-		'CWP.SPECIFYALTTEXT': 'Please specify alternative text for %s'
+		'CWP.SPECIFYALTTEXT': '%s has no alt text which is required for non-decorative images.\n\nAre you sure you want to update?'
 	});
 }
