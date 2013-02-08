01a4f4cf55ff7c92c24c4bca41110bc089daae83
Mateusz Uzdowski <mateusz@silverstripe.com>
2013-02-08 16:38:06 +1300

    Fix and update tests for EventHolder.

--- tests/EventHolderTest.php
+++ tests/EventHolderTest.php
@@ -33,30 +33,47 @@ class EventHolderTest extends SapphireTest {
 		$this->assertNull($items->find('URLSegment', 'past-event-1'), 'Does not find events at other dates.');
 	}
 
+	function testEventsWithDateRangeFilter() {
+		$holder = $this->objFromFixture('EventHolder', 'EventHolder1');
+
+		$items = $holder->Events(null, '2013-01-19', null);
+		$this->assertNotNull($items->find('URLSegment', 'past-event-2'), 'Finds the event at the date');
+		$this->assertNull($items->find('URLSegment', 'future-event-1'), 'Does not find the event at another date');
+
+		$items = $holder->Events(null, '2013-01-01', '2013-01-19');
+		$this->assertNotNull($items->find('URLSegment', 'past-event-2'), 'Finds events in the date range');
+		$this->assertNull($items->find('URLSegment', 'future-event-1'), 'Does not find event out of range');
+	}
+
 	function testExtractMonths() {
 		$holder = $this->objFromFixture('EventHolder', 'EventHolder1');
 
 		$months = EventHolder::ExtractMonths(
 			$holder->Events(),
-			'http://mybase.org/?tag=12', // Used for link generation
+			'http://mybase.org/?tag=12&start=10&from=2010-10-10&to=2010-10-11', // Used for link generation
 			2013, // Currently selected
-			7 // Currently selected
+			1 // Currently selected
 		);
 
+		// Check which years are generated.
 		$this->assertNotNull($months->find('YearName', 2013), 'Generates existing year');
 		$this->assertNull($months->find('YearName', 1990), 'Does not generate non-present year');
 
 		$year = $months->find('YearName', 2013);
 
+		// Check which months come up in 2013
 		$this->assertNotNull($year['Months']->find('MonthNumber', 7), 'Generates existing month');
 		$this->assertNull($year['Months']->find('MonthNumber', 12), 'Does not generate non-present month');
 
 		$month = $year['Months']->find('MonthNumber', 7);
 		$this->assertEquals(
 			$month['MonthLink'],
-			'http://mybase.org/?tag=12&year=2013&month=7&start=0',
-			'Link is built properly'
+			'http://mybase.org/?tag=12&from=2010-10-10&to=2010-10-11&month=7&year=2013',
+			'Selection link is built properly - start is removed, and tag, from and to retained.'
 		);
+
+		// Check if these months are marked properly.
+		$month = $year['Months']->find('MonthNumber', 1);
 		$this->assertEquals($month['Active'], true, 'Correctly marks active link');
 	}
 }
