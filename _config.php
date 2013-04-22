025ce3a45a4f101c56c68d7a9915b37a4b703267
Sean Harvey <sean@silverstripe.com>
2013-04-23 11:59:36 +1200

    removing forceSSL code from cwp config, goes into project _config instead

--- _config.php
+++ _config.php
@@ -3,16 +3,6 @@
  * General configuration.
  ********************************************************************************/
 
-// redirect some requests to the secure domain
-if(defined('CWP_SECURE_DOMAIN') && @$_SERVER['HTTP_X_FORWARDED_PROTOCOL'] != 'https') {
-	Director::forceSSL(array('/^Security/'), CWP_SECURE_DOMAIN);
-	// Note 1: the platform always redirects "/admin" to CWP_SECURE_DOMAIN regardless of what you set here.
-	// Note 2: if you have your own certificate installed, you can use your own domain, just omit the second parameter:
-	//   Director::forceSSL(array('/^Security/'));
-	//
-	// See Director::forceSSL for more information.
-}
-
 LeftAndMain::require_css('cwp/css/custom.css');
 
 SiteConfig::add_extension('CustomSiteConfig');
