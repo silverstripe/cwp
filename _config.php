56ef0904be0936ed961f3271cef6d7bbe70542a5
Sean Harvey <sean@silverstripe.com>
2013-04-23 12:30:38 +1200

    Revert isDev() check, because cwp is loaded before mysite where the environment is loaded

--- _config.php
+++ _config.php
@@ -149,15 +149,12 @@ $cwpEditor->addButtonsToLine(3, 'cite', 'abbr', 'ins', 'del', 'separator', 'tabl
 TaxonomyTerm::add_extension('TaxonomyTermExtension');
 
 // configure administrative logging
-// don't log when in dev mode
-if(!Director::isDev()) {
-	$logFileWriter = new SS_SysLogWriter('SilverStripe', null, LOG_AUTH);
-	$logFileWriter->setFormatter(new CwpLoggerFormatter());
-	SS_Log::add_writer($logFileWriter, CwpLogger::PRIORITY, '=');
-	MemberLoginForm::add_extension('CwpLogger');
-	Security::add_extension('CwpLogger');
-	RequestHandler::add_extension('CwpLogger');
-	Controller::add_extension('CwpLogger');
-	Member::add_extension('CwpLogger');
-}
+$logFileWriter = new SS_SysLogWriter('SilverStripe', null, LOG_AUTH);
+$logFileWriter->setFormatter(new CwpLoggerFormatter());
+SS_Log::add_writer($logFileWriter, CwpLogger::PRIORITY, '=');
+MemberLoginForm::add_extension('CwpLogger');
+Security::add_extension('CwpLogger');
+RequestHandler::add_extension('CwpLogger');
+Controller::add_extension('CwpLogger');
+Member::add_extension('CwpLogger');
 
