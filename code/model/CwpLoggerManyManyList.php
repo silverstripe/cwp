3771057d419a20ac189a6c2e55c439483402cdde
Sean Harvey <sean@silverstripe.com>
2013-05-22 09:10:59 +1200

    Removing unnecessary code in CwpLoggerManyManyList

--- code/model/CwpLoggerManyManyList.php
+++ code/model/CwpLoggerManyManyList.php
@@ -8,16 +8,10 @@ class CwpLoggerManyManyList extends ManyManyList {
 	public function removeByID($itemID) {
 		parent::removeByID($itemID);
 
-		$currentMember = Member::currentUser();
-		if(!($currentMember && $currentMember->exists())) return;
+		if($this->getJoinTable() == 'Group_Members') {
+			$currentMember = Member::currentUser();
+			if(!($currentMember && $currentMember->exists())) return;
 
-		$class = $this->dataClass();
-		$data = $class::get()->byId($itemID);
-		if(!$data) return;
-
-		$joinTable = $this->getJoinTable();
-
-		if($joinTable == 'Group_Members') {
 			$group = Group::get()->byId($itemID);
 			$member = Member::get()->byId($this->getForeignID());
 
