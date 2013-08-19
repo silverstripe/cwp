123a62bdf0921665c1bc92d89a41d44884c75c85
Mateusz Uzdowski <mateusz@silverstripe.com>
2013-08-19 17:06:37 +1200

    Split the functionality into core and features. Features stay here.

--- code/extensions/CustomHtmlEditorField.php
+++
@@ -1,9 +0,0 @@
-<?php
-class CustomHtmlEditorFieldToolbar extends Extension {
-
-	public function updateMediaForm($form) {
-		Requirements::add_i18n_javascript('cwp/javascript/lang');
-		Requirements::javascript('cwp/javascript/CustomHtmlEditorFieldToolbar.js');
-	}
-
-}
