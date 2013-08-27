233b5f8b9ee46896d561aa0135b6fe5dc2e05279
Naomi Guyer <naomi@silverstripe.com>
2013-08-27 16:16:32 +1200

    FEATURE: Translate language nav
    Provides naivigation labels in native language, and adds a link to the
    homepage when there is a translated homepage, but no translated version
    of the current page

--- code/pagetypes/BasePage.php
+++ code/pagetypes/BasePage.php
@@ -158,6 +158,62 @@ class BasePage extends SiteTree {
 		return $fields;
 	}
 
+	/**
+	 * Provides data for translation navigation.
+	 * Collects all site translations, marks the current one, and redirects
+	 * to the translated home page if a. there is a translated homepage and b. the 
+	 * translation of the specific page is not available.
+	 */
+	function getAvailableTranslations() {
+		
+		if(!class_exists('Translatable')){
+			return false;
+		}
+
+		$translations = new ArrayList();
+		$globalTranslations = Translatable::get_existing_content_languages();
+
+		foreach ($globalTranslations as $loc=>$langName) {
+
+			// Find out the language name in native language.
+			$nativeLangName = i18n::get_language_name($loc, true);
+			if (!$nativeLangName) {
+				$nativeLangName = i18n::get_language_name(i18n::get_lang_from_locale($loc), true);
+			}
+			if (!$nativeLangName) {
+				// Fall back to the locale name.
+				$nativeLangName = $langName;
+			}
+
+			// Eliminate the part in brackets (e.g. [mandarin])
+			$nativeLangName = preg_replace('/ *[\(\[].*$/', '', $nativeLangName);
+
+			// Find out the link to the translated page.
+			$link = null;
+			$page = $this->getTranslation($loc);
+			if ($page) {
+				$link = $page->Link();
+			}
+			if (!$link) {
+				// Fall back to the home page
+				$link = Translatable::get_homepage_link_by_locale($loc);
+			}
+			if (!$link) {
+				continue;
+			}
+
+			// Assemble the table for the switcher.
+			$translations->push(new ArrayData(array(
+				'LangName' => $nativeLangName,
+				'Link' => $link,
+				'Current' => (Translatable::get_current_locale()==$loc)
+			)));
+		}
+
+		if ($translations->count()>1) return $translations;
+		else return null;
+	}
+
 }
 
 class BasePage_Controller extends ContentController {
