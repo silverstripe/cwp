d57fb4192ca5eb8280823ead54e48d75bd480bc8
Damian Mooyman <damian@silverstripe.com>
2015-06-30 11:36:26 +1200

    Add boost search extension

---
+++ code/extensions/CwpSearchBoostExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Adds field boosting capabilities to fulltext search for pages
+ */
+class CwpSearchBoostExtension extends DataExtension {
+	
+	/**
+	 * Quality to boost the 'SearchBoost' field by.
+	 * Default boost is 2x
+	 *
+	 * @var config
+	 * @string 
+	 */
+	private static $search_boost = '2';
+	
+	private static $db = array(
+		'SearchBoost' => 'Text'
+	);
+
+	/**
+	 * Adds boost fields to this page
+	 *
+	 * @param FieldList $fields
+	 */
+	public function updateCMSFields(FieldList $fields) {
+		parent::updateCMSFields($fields);
+		
+		$boostTitle = _t('CwpSiteTreeSearchBoost.SearchBoost', 'Search Keywords');
+		$boostNote = _t('CwpSiteTreeSearchBoost.SearchBoostNote', '(Only applies to the search results on this site e.g. not on Google search)');
+		$boostDescription = _t(
+			'CwpSiteTreeSearchBoost.SearchBoostDescription',
+			'Enter keywords separated by comma ( , ) for which to boost the ranking of this page within the search results on this site.'
+		);
+		$boostField = TextareaField::create('SearchBoost', $boostTitle)
+			->setRightTitle($boostNote)
+			->setDescription($boostDescription);
+		$fields->insertBefore($boostField, 'MetaDescription');
+	}
+}
