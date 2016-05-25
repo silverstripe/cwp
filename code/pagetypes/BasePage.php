8d50309623e2dfeb39a7a98ceee13324d15956f6
Jake Ovenden <jake.dale@xtra.co.nz>
2016-05-25 14:02:42 +1200

    add alternate_protocol back in

--- code/pagetypes/BasePage.php
+++ code/pagetypes/BasePage.php
@@ -332,6 +332,7 @@ class BasePage_Controller extends ContentController {
 		// Force http protocol on CWP and ensure a domain which supports https is used - fetching from localhost without using the proxy, SSL terminates on gateway.
 		if (defined('CWP_ENVIRONMENT')) {
 			Config::inst()->nest();
+			Config::inst()->update('Director', 'alternate_protocol', 'http');
 			Config::inst()->update('Director', 'alternate_base_url', 'http://'.CWP_SECURE_DOMAIN.'/');
 		}
 
