ecada7847528db96db73e7bafb4579816eb9d10e
Jake Ovenden <jake.dale@xtra.co.nz>
2016-08-15 17:52:44 +1200

    update default binary to use the most recent

--- _config.php
+++ _config.php
@@ -2,5 +2,5 @@
 
 // default to the binary being in the usual path on Linux
 if(!defined('WKHTMLTOPDF_BINARY')) {
-	define('WKHTMLTOPDF_BINARY', '/usr/local/bin/wkhtmltopdf');
+	define('WKHTMLTOPDF_BINARY', '/usr/local/bin/wkhtmltopdf_12');
 }
